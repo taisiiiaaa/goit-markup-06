550ba49cac7d96ba91317aeb78d1ce8959fab9ae
Taisiia Shloma <shloma20082004@icloud.com>
2026-04-13 15:30:39 +0200

    add base styles for mobile

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    input: document.querySelector('#name'),
+    burger: document.querySelector('[data-mobile-menu-open]'),
+    mobileMenu: document.querySelector('[data-mobile-menu]'),
+    closeMobileMenuBtn: document.querySelector('[data-mobile-menu-close]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.burger.addEventListener('click', toggleMobileMenu);
+  refs.closeMobileMenuBtn.addEventListener('click', toggleMobileMenu);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
-    refs.input.focus();
+  }
+
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle('is-open');
   }
 })();
